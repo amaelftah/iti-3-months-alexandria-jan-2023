5f7071032ed8481bb1278bb3f7c3725049a4b2fc
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-01-28 11:58:50 +0200

    Add show all posts

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,14 +8,25 @@ class TestController extends Controller
 {
     public function test()
     {
-        $localBooks = [
-            'Book 1',
-            'Book 2',
+        $allPosts = [
+            [
+                'id' => 1,
+                'title' => 'laravel',
+                'description' => 'hello this is laravel post',
+                'posted_by' => 'Ahmed',
+                'created_at' => '2022-01-28 10:05:00',
+            ],
+            [
+                'id' => 2,
+                'title' => 'php',
+                'description' => 'hello this is php post',
+                'posted_by' => 'Mohamed',
+                'created_at' => '2022-01-30 10:05:00',
+            ],
         ];
-
+//        dd($allPosts);
         return view('test',[
-            'name' => 'ahmed',
-            'books' => $localBooks
+            'posts' => $allPosts,
         ]);
     }
 }
